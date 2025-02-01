84c2e53fc9690766bc4914e07f8f1de7a7089901
Uhanovitage Waruna Udara Sampath <warunaudarasam2003@gmail.com>
2025-02-02 02:37:30 +0530

    nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/Hero";
-
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItemsData } from "@/data/data";
 
 export default function Home() {
   return (
     <div className="">
+      <FloatingNav navItems={navItemsData} />
       <Hero/>
     </div>
   );
---
+++ data/data.ts
@@ -0,0 +1,9 @@
+"use client";
+export const navItemsData = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+
+  export const heroData:string="Engineering Innovative Solutions";
